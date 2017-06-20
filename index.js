5bca1135482a8dd9a6679852dc97a153d66bda89
Quentin DEBESSON <quentin.debesson@viseo.com>
2017-06-20 17:10:29 +0200

    Externalize env global variables

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ let Slack = require('@slack/client');
 const Game = require('./game/rockpaperscissors');
 const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 const Player = require('./game/player');
+const SlackEnv = require('./.env/environment').GLOBAL_VARIABLES;
 
-let token = 'xoxb-168088437603-eHxSPkdy5LpRczuTd9H733zS';
-let channelId = 'C4XD97YCQ';
-
-let rtm = new Slack.RtmClient(token, {logLevel: 'error', dataStore: new Slack.MemoryDataStore()});
+let rtm = new Slack.RtmClient(SlackEnv.token, {logLevel: 'error', dataStore: new Slack.MemoryDataStore()});
 
 let users = [];
 let game = null;
@@ -17,7 +15,7 @@ rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, () => {
 
 rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, () => {
   console.log('In !');
-  rtm.sendMessage('Hello. Time is currently ' + new Date().toDateString(), channelId);
+  rtm.sendMessage('Hello. Time is currently ' + new Date().toDateString(), SlackEnv.channelId);
 });
 
 rtm.on(Slack.RTM_EVENTS.MESSAGE, (message) => {
@@ -28,12 +26,12 @@ rtm.on(Slack.RTM_EVENTS.MESSAGE, (message) => {
       if (users.length < 2) {
         if (users.filter(user => user.id === message.user).length === 0) {
           users.push(new Player(user.id, user.name, user.dm.id, rtm));
-          rtm.sendMessage(user.name + ', vous avez rejoint la partie !', channelId);
+          rtm.sendMessage(user.name + ', vous avez rejoint la partie !', SlackEnv.channelId);
           if (users.length === 2) {
             startGame();
           }
         } else {
-          rtm.sendMessage(user.name + ', vous avez déjà rejoint la partie !', channelId);
+          rtm.sendMessage(user.name + ', vous avez déjà rejoint la partie !', SlackEnv.channelId);
         }
       }
     } else if(game && game.started && (message.text === Game.Rock || message.text === Game.Paper || message.text === Game.Scissors)) {
@@ -42,7 +40,7 @@ rtm.on(Slack.RTM_EVENTS.MESSAGE, (message) => {
       if(game && game.started) {
         users = game.removePlayer(message.user);
       } else if(users && users[0]) {
-        rtm.sendMessage(users[0].name + ' a quitté la partie ! Qui veut jouer ? :)', channelId);
+        rtm.sendMessage(users[0].name + ' a quitté la partie ! Qui veut jouer ? :)', SlackEnv.channelId);
         users = [];
       }
     }
@@ -52,6 +50,6 @@ rtm.on(Slack.RTM_EVENTS.MESSAGE, (message) => {
 rtm.start();
 
 let startGame = () => {
-  game = new Game.RockPaperScissors(users, channelId, rtm);
+  game = new Game.RockPaperScissors(users, SlackEnv.channelId, rtm);
   game.startGame();
 };
